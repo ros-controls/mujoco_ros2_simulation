c37ec5e94f11a84ff8a9f7b5bb921e0817bd0de7
Nathan Dunkelberger <nathan.b.dunkelberger@nasa.gov>
2025-11-24 09:45:16 -0600

    adding ros2 control taskbar icon on startup

--- src/mujoco_system_interface.cpp
+++ src/mujoco_system_interface.cpp
@@ -35,9 +35,15 @@
 #include <string>
 #include <thread>
 
+<<<<<<< Updated upstream
+=======
+#include <tinyxml2.h>
+#include <ament_index_cpp/get_package_share_directory.hpp>
+>>>>>>> Stashed changes
 #include <hardware_interface/types/hardware_interface_type_values.hpp>
 #include <pluginlib/class_list_macros.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include "lodepng.h"
 
 #define MUJOCO_PLUGIN_DIR "mujoco_plugin"
 
@@ -368,6 +374,33 @@ MujocoSystemInterface::on_init(const hardware_interface::HardwareComponentInterf
   ui_thread_ = std::thread([this, sim_ready]() {
     sim_ = std::make_unique<mj::Simulate>(std::make_unique<mj::GlfwAdapter>(), &cam_, &opt_, &pert_,
                                           /* is_passive = */ false);
+
+    // Add ros2 control icon for the taskbar
+    std::string icon_location =
+        ament_index_cpp::get_package_share_directory("mujoco_ros2_simulation") + "/resources/logo_ros-controls.png";
+    std::vector<unsigned char> image;
+    unsigned width, height;
+    unsigned error = lodepng::decode(image, width, height, icon_location);
+
+    // Only process the icon if we successfully loaded it. Otherwise, just proceed without
+    if (error)
+    {
+      RCLCPP_WARN_STREAM(rclcpp::get_logger("MujocoSystemInterface"),
+                         "LodePNG error " << error << ": " << lodepng_error_text(error)
+                                          << ". Icon file not loaded: " << icon_location);
+    }
+    else
+    {
+      GLFWimage icon;
+      icon.width = width;
+      icon.height = height;
+      icon.pixels = image.data();
+
+      GLFWwindow* window_pointer = glfwGetCurrentContext();
+
+      glfwSetWindowIcon(window_pointer, 1, &icon);
+    }
+
     // Notify sim that we are ready
     sim_ready->set_value();
 
