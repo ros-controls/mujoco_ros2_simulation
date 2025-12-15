c04b856f2440f88e41cac8565b22db914b76b7dc
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-12-15 22:20:31 +0100

    Add first set of tests

---
+++ test/src/robot_launch_test.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 PAL Robotics S.L.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import time
+import unittest
+
+from ament_index_python.packages import get_package_share_directory
+from controller_manager.test_utils import (
+    check_controllers_running,
+    check_if_js_published,
+    check_node_running
+)
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_testing.actions import ReadyToTest
+from launch_testing.util import KeepAliveProc
+from launch_testing_ros import WaitForTopics
+import pytest
+import rclpy
+from rosgraph_msgs.msg import Clock
+from std_msgs.msg import Float64MultiArray
+from sensor_msgs.msg import JointState
+
+
+# This function specifies the processes to be run for our test
+def generate_test_description_common(use_pid='false'):
+    # This is necessary to get unbuffered output from the process under test
+    proc_env = os.environ.copy()
+    proc_env['PYTHONUNBUFFERED'] = '1'
+    launch_include = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('mujoco_ros2_simulation'),
+                'launch/test_robot.launch.py',
+            )
+        ),
+        launch_arguments={'headless': 'true', 'use_pid': use_pid}.items(),
+    )
+
+    return LaunchDescription([launch_include, KeepAliveProc(), ReadyToTest()])
+
+@pytest.mark.rostest
+def generate_test_description():
+    return generate_test_description_common(use_pid='false')
+
+
+class TestFixture(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        rclpy.init()
+
+    @classmethod
+    def tearDownClass(cls):
+        rclpy.shutdown()
+
+    def setUp(self):
+        self.node = rclpy.create_node('test_node')
+
+    def tearDown(self):
+        self.node.destroy_node()
+
+    def test_node_start(self, proc_output):
+        check_node_running(self.node, 'robot_state_publisher')
+
+    def test_clock(self):
+        topic_list = [('/clock', Clock)]
+        with WaitForTopics(topic_list, timeout=10.0):
+            print('/clock is receiving messages!')
+
+    def test_check_if_msgs_published(self):
+        check_if_js_published(
+            '/joint_states',
+            [
+                'joint1',
+                'joint2',
+            ],
+        )
+
+    def test_arm(self, launch_service, proc_info, proc_output):
+
+        # Check if the controllers are running
+        cnames = ['position_controller', 'joint_state_broadcaster']
+        check_controllers_running(self.node, cnames)
+
+        # Create a publisher to send commands to the position controller
+        pub = self.node.create_publisher(Float64MultiArray, '/position_controller/commands', 10)
+
+        # Wait for subscriber to connect
+        end_time = time.time() + 10
+        while time.time() < end_time:
+            if pub.get_subscription_count() > 0:
+                break
+            time.sleep(0.1)
+
+        self.assertGreater(pub.get_subscription_count(), 0, "Controller did not subscribe to commands")
+
+        msg = Float64MultiArray()
+        msg.data = [0.5, -0.5]
+        pub.publish(msg)
+
+        # Allow some time for the message to be processed
+        time.sleep(2.0)
+
+        # Now, check that the joint states have been updated accordingly
+        joint_state_topic = '/joint_states'
+        wait_for_topics = WaitForTopics([(joint_state_topic, JointState)], timeout=20.0)
+        assert wait_for_topics.wait(), f"Topic '{joint_state_topic}' not found!"
+        msgs = wait_for_topics.received_messages(joint_state_topic)
+        msg = msgs[0]
+        assert len(msg.name) >= 2, "Joint states message doesn't have 2 joints"
+        joint1_index = msg.name.index('joint1')
+        joint2_index = msg.name.index('joint2')
+        self.assertAlmostEqual(msg.position[joint1_index], 0.5, delta=0.05, msg="joint1 did not reach the commanded position")
+        self.assertAlmostEqual(msg.position[joint2_index], -0.5, delta=0.05, msg="joint2 did not reach the commanded position")
+
