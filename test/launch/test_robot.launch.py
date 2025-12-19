4a8798ba462bc448dfb34998ccb066161b232ee1
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2025-12-19 14:08:13 +0100

    parameterize URDF to work with use_mjcf_from_topic argument

--- test/launch/test_robot.launch.py
+++ test/launch/test_robot.launch.py
@@ -26,9 +26,9 @@ from launch.substitutions import (
     PathJoinSubstitution,
 )
 from launch_ros.actions import Node
+from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.parameter_descriptions import ParameterValue, ParameterFile
 from launch_ros.substitutions import FindPackageShare
-from launch.actions import ExecuteProcess
 
 
 def generate_launch_description():
@@ -39,6 +39,12 @@ def generate_launch_description():
     )
 
     headless = DeclareLaunchArgument("headless", default_value="false", description="Run in headless mode")
+
+    use_mjcf_from_topic = DeclareLaunchArgument(
+        "use_mjcf_fuse_mjcf_from_topicrom_topic",
+        default_value="false",
+        description="When set to true, the MJCF is generated at runtime from URDF",
+    )
 
     robot_description_content = Command(
         [
@@ -57,32 +63,21 @@ def generate_launch_description():
             " ",
             "headless:=",
             LaunchConfiguration("headless"),
+            " ",
+            "use_mjcf_from_topic:=",
+            LaunchConfiguration("use_mjcf_from_topic"),
         ]
     )
 
     robot_description = {"robot_description": ParameterValue(value=robot_description_content, value_type=str)}
 
-    converter_command = [
-        "python3",
-        PathJoinSubstitution(
-            [
-                FindPackageShare("mujoco_ros2_control"),
-                "scripts",
-                "make_mjcf_from_robot_description.py",
-            ]
-        ),
-        "--urdf",
-        PathJoinSubstitution(
-            [
-                FindPackageShare("mujoco_ros2_simulation"),
-                "test_resources",
-                "test_robot.urdf",
-            ]
-        ),
+    converter_arguments_without_pids = [
+        "--robot_description",
+        robot_description_content,
         "--m",
         PathJoinSubstitution(
             [
-                FindPackageShare("mujoco_ros2_simulation"),
+                FindPackageShare("mujoco_ros2_control"),
                 "test_resources",
                 "test_inputs.xml",
             ]
@@ -90,21 +85,38 @@ def generate_launch_description():
         "--scene",
         PathJoinSubstitution(
             [
-                FindPackageShare("mujoco_ros2_simulation"),
+                FindPackageShare("mujoco_ros2_control"),
                 "test_resources",
-                "scene.xml",
+                "scene_info.xml",
             ]
         ),
         "--publish_topic",
         "/mujoco_robot_description",
     ]
 
-    converter_process = ExecuteProcess(
-        cmd=converter_command,
-        name="make_mjcf_from_robot_description",
-        output="screen",
-        #   True to see the output
+    converter_arguments_with_pids = [
+        "--publish_topic",
+        "/mujoco_robot_description",
+    ]
+
+    print(converter_arguments_without_pids)
+
+    converter_node_without_pids = Node(
+        package="mujoco_ros2_control",
+        executable="make_mjcf_from_robot_description.py",
+        output="both",
         emulate_tty=True,
+        arguments=converter_arguments_without_pids,
+        condition=UnlessCondition(LaunchConfiguration("use_pid")),
+    )
+
+    converter_node_with_pids = Node(
+        package="mujoco_ros2_control",
+        executable="make_mjcf_from_robot_description.py",
+        output="both",
+        emulate_tty=True,
+        arguments=converter_arguments_with_pids,
+        condition=IfCondition(LaunchConfiguration("use_pid")),
     )
 
     controller_parameters = ParameterFile(
@@ -155,10 +167,12 @@ def generate_launch_description():
         [
             use_pid,
             headless,
+            use_mjcf_from_topic,
             robot_state_publisher_node,
             control_node,
             spawn_joint_state_broadcaster,
             spawn_position_controller,
-            converter_process,
+            converter_node_with_pids,
+            converter_node_without_pids,
         ]
     )
